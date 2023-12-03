a42bb950a09bbc1705c2096d9a4fd8b78a208c16
Emmanuel Ugwu <>
2023-12-03 17:42:22 +0100

    Initial commit

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { currentCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
+  const flagemojiToPNG = (flag) => {
+    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt()).map(char => String.fromCharCode(char-127397).toLowerCase()).join('')
+    return (<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />)
+}
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
