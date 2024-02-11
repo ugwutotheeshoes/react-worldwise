4017f5ce2064aba5bc0990f4592abde84e763d6a
Emmanuel Ugwu <32464178+ugwutotheeshoes@users.noreply.github.com>
2024-02-10 16:40:50 -0800

    Update PageNav.jsx

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -13,9 +13,6 @@ function PageNav() {
         <li>
           <NavLink to="/product">Product</NavLink>
         </li>
-        <li>
-          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
-        </li>
       </ul>
     </nav>
   );
